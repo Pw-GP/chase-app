64a25350321aadea4344c821e29a757ebc089c95
Páraic Walsh <pwalsh@graypuksand.com.au>
2026-05-22 16:33:31 +1000

    add sign-in page

---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,15 @@
+import { SignUp } from '@clerk/nextjs'
+
+export default function SignUpPage() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      background: '#f5f3ef'
+    }}>
+      <SignUp />
+    </div>
+  )
+}
